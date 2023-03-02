cf1ba715c516cf8380dd0463f154b643fe7b60ca
Nicola Gaioni <nicog.designer@gmail.com>
2023-03-02 16:04:04 +0100

    color changes, starting responsive design for mobile

---
+++ constants/Nav.js
@@ -0,0 +1,92 @@
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Nav() {
+    const [list, setList] = useState(false);
+    return (
+        <>
+            <nav id="DesktopNav">
+                <Link href="/">Home</Link>
+                <Link
+                    href="/therapien"
+                    onMouseEnter={() => setList(true)}
+                    onMouseLeave={() => setList(false)}
+                    style={{ color: list ? "var(--colorB)" : "#c9d9d9" }}
+                >
+                    Therapien
+                </Link>
+                <Link href="/literatur">Literatur</Link>
+                <Link href="/links">Links</Link>
+                <Link href="/seminare">Seminare</Link>
+                <Link href="/impressum">Impressum</Link>
+                <Link href="/kontakt">Kontakt</Link>
+                {list && (
+                    <div
+                        id="List"
+                        onMouseEnter={() => setList(true)}
+                        onMouseLeave={() => setList(false)}
+                    >
+                        <Link href="/therapien/autonome-regulationsdiagnostik">
+                            Autonome Regulationsdiagnostik
+                        </Link>
+                        <Link href="/therapien/psychokinesiologie">
+                            Psychokinesiologie
+                        </Link>
+                        <Link href="/therapien/mentalfeldtherapie">
+                            Mentalfeldtherapie
+                        </Link>
+                        <Link href="/therapien/systemische-familienaufstellung">
+                            Systemische Familienaufstellung
+                        </Link>
+                        <Link href="/therapien/energiearbeit">
+                            Energiearbeit
+                        </Link>
+                    </div>
+                )}
+            </nav>
+
+            <nav id="MobileNav">
+                <div id="HamburgerBtn"></div>
+                <div className="list">
+                    <Link href="/">Home</Link>
+                    <Link
+                        href="/therapien"
+                        onMouseEnter={() => setList(true)}
+                        onMouseLeave={() => setList(false)}
+                        style={{ color: list ? "var(--colorB)" : "#c9d9d9" }}
+                    >
+                        Therapien
+                    </Link>
+                    <Link href="/literatur">Literatur</Link>
+                    <Link href="/links">Links</Link>
+                    <Link href="/seminare">Seminare</Link>
+                    <Link href="/impressum">Impressum</Link>
+                    <Link href="/kontakt">Kontakt</Link>
+                    {list && (
+                        <div
+                            id="List"
+                            onMouseEnter={() => setList(true)}
+                            onMouseLeave={() => setList(false)}
+                        >
+                            <Link href="/therapien/autonome-regulationsdiagnostik">
+                                Autonome Regulationsdiagnostik
+                            </Link>
+                            <Link href="/therapien/psychokinesiologie">
+                                Psychokinesiologie
+                            </Link>
+                            <Link href="/therapien/mentalfeldtherapie">
+                                Mentalfeldtherapie
+                            </Link>
+                            <Link href="/therapien/systemische-familienaufstellung">
+                                Systemische Familienaufstellung
+                            </Link>
+                            <Link href="/therapien/energiearbeit">
+                                Energiearbeit
+                            </Link>
+                        </div>
+                    )}
+                </div>
+            </nav>
+        </>
+    );
+}
--- constants/layout.js
+++ constants/layout.js
@@ -1,56 +1,18 @@
 import Head from "next/head";
-import Header from "@/constants/Header/Header";
-import Footer from "@/constants/Footer/Footer";
+import Header from "@/constants/Header";
+import Footer from "@/constants/Footer";
 import Link from "next/link";
 import { useState } from "react";
+import Nav from "./Nav";
 
 export default function Layout({ children }) {
-    const [list, setList] = useState(false);
-
     return (
         <>
             <Head></Head>
             <Header />
             <section>
-                <nav>
-                    <Link href="/">Home</Link>
-                    <Link
-                        href="/therapien"
-                        onMouseEnter={() => setList(true)}
-                        onMouseLeave={() => setList(false)}
-                        style={{ color: list ? "#74a099" : "#c9d9d9" }}
-                    >
-                        Therapien
-                    </Link>
-                    <Link href="/literatur">Literatur</Link>
-                    <Link href="/links">Links</Link>
-                    <Link href="/seminare">Seminare</Link>
-                    <Link href="/impressum">Impressum</Link>
-                    <Link href="/kontakt">Kontakt</Link>
-                    {list && (
-                        <div
-                            id="List"
-                            onMouseEnter={() => setList(true)}
-                            onMouseLeave={() => setList(false)}
-                        >
-                            <Link href="/therapien/autonome-regulationsdiagnostik">
-                                Autonome Regulationsdiagnostik
-                            </Link>
-                            <Link href="/therapien/psychokinesiologie">
-                                Psychokinesiologie
-                            </Link>
-                            <Link href="/therapien/mentalfeldtherapie">
-                                Mentalfeldtherapie
-                            </Link>
-                            <Link href="/therapien/systemische-familienaufstellung">
-                                Systemische Familienaufstellung
-                            </Link>
-                            <Link href="/therapien/energiearbeit">
-                                Energiearbeit
-                            </Link>
-                        </div>
-                    )}
-                </nav>
+                <Nav />
+                <nav></nav>
                 {children}
                 <Footer />
             </section>
