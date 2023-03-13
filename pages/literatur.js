67c942d99ba8082efa8a2ce1488198d60e468247
Nicola Gaioni <nicog.designer@gmail.com>
2023-03-13 21:01:07 +0100

    literatur + title changes

--- pages/literatur.js
+++ pages/literatur.js
@@ -14,45 +14,65 @@ export default function Literatur() {
             <div className="content">
                 <ul>
                     <li>
-                        Dr. Frank Liebke: „Doktor Chlorella! Die Alge fürs
-                        Leben. Kompendium zur Mikroalge Chlorella“, Remerc &
-                        Lheiw Verlagskontor, 2007.
+                        Dr. Frank Liebke:{" "}
+                        <i>
+                            „Doktor Chlorella! Die Alge fürs Leben. Kompendium
+                            zur Mikroalge Chlorella“
+                        </i>
+                        , Remerc & Lheiw Verlagskontor, 2007.
                     </li>
                     <li>
-                        Dr. Joachim Mutter: „Amalgam-Risiko für die Menschheit“,
-                        Fit fürs Leben Verlag, Weil der Stadt 2018.
+                        Dr. Joachim Mutter:{" "}
+                        <i>„Amalgam-Risiko für die Menschheit“</i>, Fit fürs
+                        Leben Verlag, Weil der Stadt 2018.
                     </li>
                     <li>
-                        Fritz Albert Popp: „Biophotonen-Neue Horizonte der
-                        Medizin: Von den Grundlagen der Biophotonik“, Haug
-                        Verlag, Stuttgart 2006.
+                        Fritz Albert Popp:{" "}
+                        <i>
+                            „Biophotonen-Neue Horizonte der Medizin: Von den
+                            Grundlagen der Biophotonik“
+                        </i>
+                        , Haug Verlag, Stuttgart 2006.
                     </li>
                     <li>
-                        Wolfgang Maes: „Stress durch Strom und Strahlung“, 6.
-                        Auflage, Institut für Baubiologie + Nachhaltigkeit,
+                        Wolfgang Maes: <i>„Stress durch Strom und Strahlung“</i>
+                        , 6. Auflage, Institut für Baubiologie + Nachhaltigkeit,
                         Rosenheim 2013.
                     </li>
                     <li>
-                        Dr. med. D. Klinghardt, Amelie Schmeer-Maurer: „Handbuch
-                        der MentalfeldTechniken“, VAK Verlag, Kirchzarten bei
-                        Freiburg 2009.
+                        Dr. med. D. Klinghardt, Amelie Schmeer-Maurer:{" "}
+                        <i>„Handbuch der MentalfeldTechniken“</i>, VAK Verlag,
+                        Kirchzarten bei Freiburg 2009.
                     </li>
                     <li>
-                        Hunter Beaumont: „Auf die Seele schauen: Spirituelle
-                        Psychotherapie“, Kösel-Verlag, München 2008.
+                        Hunter Beaumont:{" "}
+                        <i>
+                            „Auf die Seele schauen: Spirituelle Psychotherapie“
+                        </i>
+                        , Kösel-Verlag, München 2008.
                     </li>
                     <li>
-                        Gunthard Weber (Hrsg.): „Zweierlei Glück. Das klassische
-                        Familienstellen Bert Hellingers“, 18. Auflage, Carl-Auer
-                        Verlag, Heidelberg 2017.
+                        Gunthard Weber (Hrsg.):{" "}
+                        <i>
+                            „Zweierlei Glück. Das klassische Familienstellen
+                            Bert Hellingers“
+                        </i>
+                        , 18. Auflage, Carl-Auer Verlag, Heidelberg 2017.
                     </li>
                     <li>
-                        John Bradshaw: „Familiengeheimnisse. Warum es sich lohnt
-                        ihnen auf die Spur zu kommen“, Goldmann, München 2014.
+                        John Bradshaw:{" "}
+                        <i>
+                            „Familiengeheimnisse. Warum es sich lohnt ihnen auf
+                            die Spur zu kommen“
+                        </i>
+                        , Goldmann, München 2014.
                     </li>
                     <li>
-                        Colin C. Tipping: „Ich vergebe: Der radikale Abschied
-                        vom Opferdasein“, J. Kamphausen, Bielefeld 2010.{" "}
+                        Colin C. Tipping:{" "}
+                        <i>
+                            „Ich vergebe: Der radikale Abschied vom Opferdasein“
+                        </i>
+                        , J. Kamphausen, Bielefeld 2010.
                     </li>
                 </ul>
             </div>
