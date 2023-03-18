c08f498fb62643066f0d33ece23e32f7925ceaf0
Nicola Gaioni <nicog.designer@gmail.com>
2023-03-18 19:23:18 +0100

    contact form - testing

--- next.config.js
+++ next.config.js
@@ -6,6 +6,13 @@ const nextConfig = {
         defaultLocale: "de",
     },
 
+    env: {
+        REACT_AWS_KEY: process.env.REACT_AWS_KEY,
+        REACT_AWS_SECRET: process.env.REACT_AWS_SECRET,
+        RECAPTCHA_PUBLIC_KEY: process.env.RECAPTCHA_PUBLIC_KEY,
+        RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY,
+    },
+
     reactStrictMode: true,
 };
 
